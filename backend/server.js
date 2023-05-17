a109ba816b4bdbb83aa552a96e9b9a7b8fd81d0f
Joshua Makula <makulajosh@gmail.com>
2023-05-17 15:14:21 +0300

    Add project to MVC

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,31 @@
+require('dotenv').config()
+
 const express = require('express')
+const workoutroutes = require('./routes/workouts')
+const mongoose = require('mongoose')
 
 // express app
 const app = express()
 
-app.get('/', (req, res) => {
-    res.json({mssg: 'Welcome to the app'})
+// Middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
 })
 
-// listen for requests
-app.listen(4000, () => {
-    console.log('listening on port 4000!!!')
-})
+// Connect to DB
+mongoose.connect(process.env.MONG_URI)
+    .then(() => {
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('Connected to DB & listening on port 4000!!!', process.env.PORT)
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+app.use('/api/workouts', workoutroutes)
+

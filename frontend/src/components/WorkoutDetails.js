5e786c630bd9e4f64a13a8e46459990cf93d2c98
Joshua Makula <makulajosh@gmail.com>
2023-05-17 20:43:03 +0300

    Complete project

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,4 +1,8 @@
 
+
+// date-fns
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
 const WorkoutDetails = ({workout}) => {
 
     // const handleDelete = async () => {
@@ -14,7 +18,7 @@ const WorkoutDetails = ({workout}) => {
             <h4> {workout.title} </h4>
             <p><strong>Load (kg): </strong> {workout.load} </p>
             <p><strong>Reps: </strong> {workout.reps} </p>
-            <p> {workout.createdAt} </p>
+            <p> {formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })} </p>
             {/* <span onClick={handleDelete}>delete</span> */}
         </div>
      );
